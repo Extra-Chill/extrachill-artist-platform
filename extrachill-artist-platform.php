ba4c748a3cfd222a28767d530184cd72a281147a
Chris Huber <chubes4@users.noreply.github.com>
2026-01-28 17:33:44 -0600

    release: v1.6.6

--- extrachill-artist-platform.php
+++ extrachill-artist-platform.php
@@ -3,7 +3,7 @@
  * Plugin Name: Extra Chill Artist Platform
  * Plugin URI: https://extrachill.com
  * Description: Artist platform for musicians with profiles, link pages, analytics, and subscriber management.
- * Version: 1.6.5
+ * Version: 1.6.6
  * Author: Chris Huber
  * Author URI: https://chubes.net
  * License: GPL v2 or later
@@ -12,7 +12,7 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-define( 'EXTRACHILL_ARTIST_PLATFORM_VERSION', '1.6.5' );
+define( 'EXTRACHILL_ARTIST_PLATFORM_VERSION', '1.6.6' );
 define( 'EXTRACHILL_ARTIST_PLATFORM_PLUGIN_FILE', __FILE__ );
 define( 'EXTRACHILL_ARTIST_PLATFORM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'EXTRACHILL_ARTIST_PLATFORM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
